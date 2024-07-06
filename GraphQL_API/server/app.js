fb343ff63d1421418224e3c2dd73b9c681fc9ae7
cpmoore0317 <cpmoore317@gmail.com>
2024-07-06 15:07:23 -0500

    Implementing mongoDB Atlas to create mongoose models

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const schema = require('./schema/schema'); // Update the path if necessary
 
 const app = express();
 
+// Replace the following string with your MongoDB connection string
+const dbURI = 'mongodb+srv://cpmoore317:Cocobutter2@@testcluster.8qe4gop.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster';
+
+// Connect to MongoDB
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
+
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: true, // Enable the GraphiQL interface for testing queries
+  schema,
+  graphiql: true
 }));
 
 app.listen(4000, () => {
-  console.log('Now listening for requests on port 4000');
+  console.log('now listening for requests on port 4000');
 });
